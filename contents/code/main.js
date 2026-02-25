a81b1603521b40bf8e9e3dfe970631d4e65005b3
onenowy <onenowy@gmail.com>
2026-02-25 16:42:42 +0900

    Remove keeping position that conflict with plasma 6.6

--- contents/code/main.js
+++ contents/code/main.js
@@ -3,13 +3,10 @@ workspace.windowAdded.connect(function(client) {
         client.caption.includes("Picture-in-Picture")) {
         client.keepAbove = true;
         var aspectRatio = client.width / client.height;
-        var prevX, prevY;
         var prevWidth = client.width;
         var prevHeight = client.height;
 
         client.interactiveMoveResizeStarted.connect(function() {
-            prevX = client.x;
-            prevY = client.y;
             prevWidth = client.width;
             prevHeight = client.height;
         });
@@ -32,37 +29,9 @@ workspace.windowAdded.connect(function(client) {
                 newWidth = Math.round(client.height * aspectRatio);
             }
 
-            var finalX = client.x;
-            var finalY = client.y;
-
-            if (client.x !== prevX) {
-                finalX = prevX + prevWidth - newWidth;
-            }
-            if (client.y !== prevY) {
-                finalY = prevY + prevHeight - newHeight;
-            }
-
-            var screen = workspace.clientArea(workspace.PlacementArea, client);
-
-            // Clamp to right/bottom boundaries
-            if (finalX + newWidth > screen.x + screen.width) {
-                finalX = screen.x + screen.width - newWidth;
-            }
-            if (finalY + newHeight > screen.y + screen.height) {
-                finalY = screen.y + screen.height - newHeight;
-            }
-
-            // Clamp to left/top boundaries
-            if (finalX < screen.x) {
-                finalX = screen.x;
-            }
-            if (finalY < screen.y) {
-                finalY = screen.y;
-            }
-
             client.frameGeometry = {
-                x: finalX,
-                y: finalY,
+                x: client.x,
+                y: client.y,
                 width: newWidth,
                 height: newHeight
             };
